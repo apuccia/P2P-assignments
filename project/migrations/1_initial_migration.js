8947405548ac87faa5ef224fb37c58a29bc2635a
apuccia <alessandropuccia97@gmail.com>
2021-07-06 19:37:53 +0200

    added log for soul/contract address

--- project/migrations/1_initial_migration.js
+++ project/migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
 const Mayor = artifacts.require("Mayor");
 
-module.exports = function (deployer, network, accounts) {
-  deployer.deploy(Mayor, accounts.slice(0, 2), accounts[9], 9);
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(Mayor, accounts.slice(0, 2), accounts[9], 9);
+
+  const mayorIstance = await Mayor.deployed();
+  const soulAddress = await mayorIstance.token();
+
+  // print contract addresses 
+  // soul contract address is needed to import the token in metamask
+  console.log("Mayor contract address: " + mayorIstance);
+  console.log("Soul contract address: " + soulAddress);
 };
