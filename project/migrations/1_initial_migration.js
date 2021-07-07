1528de6ebdef7e9cb384d5a262c7002fb7e2476a
apuccia <alessandropuccia97@gmail.com>
2021-07-07 20:27:14 +0200

    added initial migration file

--- project/migrations/1_initial_migration.js
+++ project/migrations/1_initial_migration.js
@@ -1,27 +1,6 @@
-const Mayor = artifacts.require("Mayor");
-const SOUToken = artifacts.require("SOUToken");
+var Migrations = artifacts.require("Migrations");
 
-module.exports = async function (deployer, network, accounts) {
-  const candidates = accounts.slice(0, 2);
-  const escrow = accounts[9];
-  const quorum = 1;
-
-  await deployer.deploy(SOUToken);
-  const soulInstance = await SOUToken.deployed();
-
-  await deployer.deploy(
-    Mayor,
-    candidates,
-    escrow,
-    quorum,
-    soulInstance.address
-  );
-  const mayorInstance = await Mayor.deployed();
-
-  // print contract address and other infos
-  console.log("Mayor contract address: " + mayorInstance.address);
-  console.log("SOUToken contract address: " + soulInstance.address);
-  console.log("The candidates are " + candidates);
-  console.log("Escrow account: " + escrow);
-  console.log("Quorum: " + quorum);
+module.exports = function (deployer) {
+  // Deploy the Migrations contract as our only task
+  deployer.deploy(Migrations);
 };
---
+++ project/migrations/2_mayorsoul_migration.js
@@ -0,0 +1,28 @@
+const Mayor = artifacts.require("Mayor");
+const SOUToken = artifacts.require("SOUToken");
+
+module.exports = async function (deployer, network, accounts) {
+  const candidates = accounts.slice(0, 2);
+  const escrow = accounts[9];
+  const quorum = 1;
+
+  await deployer.deploy(SOUToken, { from: escrow });
+  const soulInstance = await SOUToken.deployed();
+
+  await deployer.deploy(
+    Mayor,
+    candidates,
+    escrow,
+    quorum,
+    soulInstance.address,
+    { from: escrow }
+  );
+  const mayorInstance = await Mayor.deployed();
+
+  // print contract address and other infos
+  console.log("Mayor contract address: " + mayorInstance.address);
+  console.log("SOUToken contract address: " + soulInstance.address);
+  console.log("The candidates are " + candidates);
+  console.log("Escrow account: " + escrow);
+  console.log("Quorum: " + quorum);
+};
