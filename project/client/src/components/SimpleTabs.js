4e823bd1a6ffb1c36a298f6448513e7788f60f84
apuccia <alessandropuccia97@gmail.com>
2021-07-08 19:18:52 +0200

    added token balance

--- project/client/src/components/SimpleTabs.js
+++ project/client/src/components/SimpleTabs.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
@@ -12,10 +13,14 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
+import Avatar from "@material-ui/core/Avatar";
 
 import ComputeEnvelopeForm from "./ComputeEnvelopeForm";
 import OpenEnvelopeForm from "./OpenEnvelopeForm";
 import ReadCandidates from "./ReadCandidates";
+import ShowResult from "./ShowResult";
+
+import soulPic from "../media/soul.png";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -64,6 +69,25 @@ function SimpleTabs(props) {
 
   const [computeEnvelope, setComputeEnvelope] = React.useState(false);
   const [openEnvelope, setOpenEnvelope] = React.useState(false);
+  const [candidates, setCandidates] = React.useState(null);
+  const [escrow, setEscrow] = React.useState(null);
+  const [balance, setBalance] = React.useState(null);
+
+  useEffect(() => {
+    const { drizzle } = props;
+    const mayorContract = drizzle.contracts.Mayor;
+    const tokenContract = drizzle.contracts.SOUToken;
+
+    const candidates = mayorContract.methods["get_candidates"].cacheCall();
+    const escrow = mayorContract.methods["escrow"].cacheCall();
+    const balance = tokenContract.methods["balanceOf"].cacheCall(
+      props.drizzleState.accounts[0]
+    );
+
+    setCandidates(candidates);
+    setEscrow(escrow);
+    setBalance(balance);
+  }, [props]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -93,35 +117,58 @@ function SimpleTabs(props) {
     setValue(false);
   };
 
+  const showBalance = () => {
+    const { SOUToken } = props.drizzleState.contracts;
+
+    if (balance == null) {
+      return null;
+    }
+
+    // using the saved `dataKey`, get the variable we're interested in
+    const balanceValue = SOUToken.balanceOf[balance];
+
+    return balanceValue && balanceValue.value / 10 ** 18;
+  };
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Grid item>
-            <IconButton onClick={handleClick} edge="start" color="inherit">
-              <MenuIcon />
-            </IconButton>
+          <Grid container spacing={3}>
+            <Grid item align="left">
+              <IconButton onClick={handleClick} edge="start" color="inherit">
+                <MenuIcon />
+              </IconButton>
 
-            <Menu
-              anchorEl={anchorEl}
-              keepMounted
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleComputeEnvelope}>
-                Compute Envelope
-              </MenuItem>
-              <MenuItem onClick={handleOpenEnvelope}>Open Envelope</MenuItem>
-            </Menu>
-          </Grid>
-          <Grid item>
-            <Typography
-              variant="h6"
-              color="inherit"
-              style={{ marginLeft: "10px" }}
-            >
-              Voting System
-            </Typography>
+              <Menu
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleComputeEnvelope}>
+                  Compute Envelope
+                </MenuItem>
+                <MenuItem onClick={handleOpenEnvelope}>Open Envelope</MenuItem>
+              </Menu>
+            </Grid>
+            <Grid item xs align="center">
+              <Typography variant="h4" color="inherit">
+                Valadilène Voting System
+              </Typography>
+            </Grid>
+            <Grid item align="right">
+              <Typography
+                variant="h5"
+                color="inherit"
+                style={{ marginTop: "5px" }}
+              >
+                {balance ? showBalance() : ""}
+              </Typography>
+            </Grid>
+            <Grid item align="right">
+              <Avatar src={soulPic} />
+            </Grid>
           </Grid>
         </Toolbar>
 
@@ -145,6 +192,15 @@ function SimpleTabs(props) {
         <ReadCandidates
           drizzle={props.drizzle}
           drizzleState={props.drizzleState}
+          candidates={candidates}
+        />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <ShowResult
+          candidates={candidates}
+          drizzle={props.drizzle}
+          drizzleState={props.drizzleState}
+          escrow={escrow}
         />
       </TabPanel>
 
