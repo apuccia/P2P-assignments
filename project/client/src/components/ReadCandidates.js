f846b543e27f01a55eebfbe619ac89877a5fdfb6
apuccia <alessandropuccia97@gmail.com>
2021-07-09 20:09:58 +0200

    added map to handle candidates

--- project/client/src/components/ReadCandidates.js
+++ project/client/src/components/ReadCandidates.js
@@ -9,27 +9,23 @@ class ReadCandidates extends React.Component {
     // get the contract state from drizzleState
     const { Mayor } = this.props.drizzleState.contracts;
 
-    const candidates = Mayor.get_candidates[this.props.candidates];
-    console.log(candidates);
+    const candidates = Mayor.get_candidates[this.props.dataKeyCandidates];
     // if it exists, then we display its value
     return (
       <Grid container spacing={10} justify="center">
-        <Grid item md={3}>
-          <Candidate
-            name={dappInfo.candidates[0].name}
-            slogan={dappInfo.candidates[0].slogan}
-            img={dappInfo.candidates[0].pic}
-            address={candidates && candidates.value[0]}
-          />
-        </Grid>
-        <Grid item md={3}>
-          <Candidate
-            name={dappInfo.candidates[1].name}
-            slogan={dappInfo.candidates[1].slogan}
-            img={dappInfo.candidates[1].pic}
-            address={candidates && candidates.value[1]}
-          />
-        </Grid>
+        {candidates &&
+          candidates.value.map(function (item, index) {
+            return (
+              <Grid key={dappInfo.candidates[index].name} item md={3}>
+                <Candidate
+                  name={dappInfo.candidates[index].name}
+                  img={dappInfo.candidates[index].pic}
+                  address={item}
+                  slogan={dappInfo.candidates[index].slogan}
+                />
+              </Grid>
+            );
+          })}
       </Grid>
     );
   }
