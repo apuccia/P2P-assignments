7376a1e7f28334d491e7620933f2c007fc880fd8
apuccia <alessandropuccia97@gmail.com>
2021-07-10 11:02:29 +0200

    removed broken from option

--- project/migrations/1_initial_migration.js
+++ project/migrations/1_initial_migration.js
@@ -3,7 +3,5 @@ var Migrations = artifacts.require("Migrations");
 module.exports = function (deployer) {
   // Deploy the Migrations contract as our only task
 
-  const escrow = accounts[9];
-
-  deployer.deploy(Migrations, { from: escrow });
+  deployer.deploy(Migrations);
 };
